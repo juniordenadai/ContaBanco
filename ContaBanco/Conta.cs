adc4dbbe0f3836f12796347b73c010d86e6759c3
Junior Denadai <Junior Denadai@DESKTOP-0N42MF1>
2021-02-16 00:13:02 -0500

    Add project files.

---
+++ ContaBanco/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBanco
+{
+    class Conta
+    {
+        private string _nome;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public const int tarifa = 5;
+
+        public Conta(int numero, string nome, double saldo)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo = saldo;
+
+        }
+
+        public Conta(int numero, string nome)
+        { 
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+
+        }
+
+        public void Deposito (double deposito)
+        {
+            Saldo = Saldo + deposito;
+
+        }
+       
+        public void Saque (double saque)
+        {
+            Saldo = Saldo - (saque + tarifa);
+
+        }
+
+    }
+
+}
---
+++ ContaBanco/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ContaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string nome;
+            char dep;
+            double saldo;
+            double deposito;
+            double saque;
+            Console.Write(" Entre com o numero da conta ");
+            numero = int.Parse(Console.ReadLine());
+            Console.Write(" Entre com o nome do Titular ");
+            nome = Console.ReadLine();
+            Console.Write("Haverá deposito inicial? (S/N) ");
+            dep = char.Parse(Console.ReadLine());
+            if (dep == 'S' || dep =='s')
+            {
+                Console.Write(" Entre com o valor inicial ");
+                saldo = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                saldo = 0.00;
+            }
+            Conta conta = new Conta(numero, nome, saldo);
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine("Conta " + conta.Numero + " Nome do Titular: " + conta.Nome + " Saldo Atual: " + conta.Saldo);
+
+            Console.Write("Valor a Depositar: ");
+            deposito = double.Parse(Console.ReadLine());
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine("Conta " + conta.Numero + " Nome do Titular: " + conta.Nome + " Saldo Atual: " + conta.Saldo);
+
+            Console.Write("Valor a Sacar: ");
+            saque = double.Parse(Console.ReadLine());
+            conta.Saque(saque);
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine("Conta " + conta.Numero + " Nome do Titular: " + conta.Nome + " Saldo Atual: " + conta.Saldo);
+
+        }
+    }
+}
